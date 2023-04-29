597ed739fd7fdb40763839c24684b7a630375531
Sadrmm <al394498@uji.es>
2023-04-30 01:33:16 +0200

    organize PC assemblies

---
+++ FIXING IT/Assets/External/ProgramadorCastellano/Scripts/UI/UISkinData/SelectableUIData.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProgramadorCastellano.UI
+{
+    [RequireComponent(typeof(Selectable))]
+    public class SelectableUIData : MonoBehaviour
+    {
+        [SerializeField] private SelectableUISkinDataSO _skinDataSO;
+
+        private Selectable _selectable;
+
+        private void Awake()
+        {
+            //_selectable = GetComponent<Selectable>();
+
+            OnSkinUI();
+        }
+
+        private void OnSkinUI()
+        {
+            _selectable = GetComponent<Selectable>();
+            //Debug.Log(_selectable.gameObject.name);
+
+            _selectable.colors = _skinDataSO.Colors;
+        }
+
+
+        // COMMENT THE UPDATE AFTER DESGIN
+        private void Update()
+        {
+            if (Application.isEditor)
+            {
+                OnSkinUI();
+            }
+        }
+    }
+}
--- FIXING IT/Assets/Scripts/UI/UISkinData/SelectableUISkinDataSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-[CreateAssetMenu(menuName = "UI/Selectable UI Skin Data")]
-public class SelectableUISkinDataSO : ScriptableObject
-{
-    [SerializeField] private ColorBlock _colors;
-    public ColorBlock Colors => _colors;
-}
