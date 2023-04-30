ba72157c4f46263764c86571c5af5c1a8fddfc51
Sadrmm <al394498@uji.es>
2023-04-30 04:35:52 +0200

    FixingIt.CharacterSelection

--- FIXING IT/Assets/Scripts/CharacterSelection/TestCharacterSelection.cs
+++ FIXING IT/Assets/Scripts/CharacterSelection/TestCharacterSelection.cs
@@ -3,61 +3,67 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-/// <summary>
-/// PONER TODO PRIVADO DESPUES DE TESTEAR
-/// EN LA VERSION FINAL QUE NO SEA SINGLETON, SOLO PARA TESTEO
-/// </summary>
-public class TestCharacterSelection : NetworkBehaviour
+namespace FixingIt.CharacterSelection
 {
-    public static TestCharacterSelection Instance { get; private set; }
-
-    [Header("Broadcasting To")]
-    [SerializeField]
-    private VoidEventChannelSO _allPlayersReadyEvent;
-    [SerializeField]
-    private VoidEventChannelSO _networkToMainMenuEvent;
-
-    private Dictionary<ulong, bool> _playerReadyDictionary;
-
-    private void Awake()
+    /// <summary>
+    /// PONER TODO PRIVADO DESPUES DE TESTEAR
+    /// EN LA VERSION FINAL QUE NO SEA SINGLETON, SOLO PARA TESTEO
+    /// </summary>
+    public class TestCharacterSelection : NetworkBehaviour
     {
-        Instance = this;
-        _playerReadyDictionary = new Dictionary<ulong, bool>();
-    }
+        public static TestCharacterSelection Instance { get; private set; }
 
-    public void SetPlayerReady()
-    {
-        SetPlayerReadyServerRpc();
-    }
+        [Header("Broadcasting To")]
+        [SerializeField]
+        private VoidEventChannelSO _allPlayersReadyEvent;
+        [SerializeField]
+        private VoidEventChannelSO _networkToMainMenuEvent;
 
-    [ServerRpc(RequireOwnership = false)]
-    private void SetPlayerReadyServerRpc(ServerRpcParams serverRpcParams = default)
-    {
-        _playerReadyDictionary[serverRpcParams.Receive.SenderClientId] = true;
+        private Dictionary<ulong, bool> _playerReadyDictionary;
 
-        // check if all clients are ready
-        bool allClientsReady = true;
-        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds) {
-            if (!_playerReadyDictionary.ContainsKey(clientId)
-                || !_playerReadyDictionary[clientId]) {
-                // this player is not ready
-                allClientsReady = false;
-                break;
+        private void Awake()
+        {
+            Instance = this;
+            _playerReadyDictionary = new Dictionary<ulong, bool>();
+        }
+
+        public void SetPlayerReady()
+        {
+            SetPlayerReadyServerRpc();
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void SetPlayerReadyServerRpc(ServerRpcParams serverRpcParams = default)
+        {
+            _playerReadyDictionary[serverRpcParams.Receive.SenderClientId] = true;
+
+            // check if all clients are ready
+            bool allClientsReady = true;
+            foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+            {
+                if (!_playerReadyDictionary.ContainsKey(clientId)
+                    || !_playerReadyDictionary[clientId])
+                {
+                    // this player is not ready
+                    allClientsReady = false;
+                    break;
+                }
+            }
+
+            if (allClientsReady)
+            {
+                // send event
+                _allPlayersReadyEvent.RaiseEvent();
             }
         }
 
-        if (allClientsReady) {
-            // send event
-            _allPlayersReadyEvent.RaiseEvent();
-        }
-    }
+        // REFACTORIZAR EN LA NO TEST
+        public void LeaveToMainMenu()
+        {
+            NetworkManager.Singleton.Shutdown();
 
-    // REFACTORIZAR EN LA NO TEST
-    public void LeaveToMainMenu()
-    {
-        NetworkManager.Singleton.Shutdown();
-        
-        // send event to change scene
-        _networkToMainMenuEvent.RaiseEvent();
+            // send event to change scene
+            _networkToMainMenuEvent.RaiseEvent();
+        }
     }
 }
