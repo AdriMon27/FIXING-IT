064c4b1ff33aaf743bf17cefd3bf89f8da17ae63
Sadrmm <al394498@uji.es>
2023-07-13 18:49:23 +0200

    final fix

--- FIXING IT/Assets/Scripts/UI/Minigame/UIEndGamePanel.cs
+++ FIXING IT/Assets/Scripts/UI/Minigame/UIEndGamePanel.cs
@@ -1,11 +1,23 @@
+using ProgramadorCastellano.Events;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace FixingIt.UI.Minigame
 {
     public class UIEndGamePanel : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _endGameText;
+        [SerializeField] private Button _toMainMenuBtn;
+
+        [Header("Broadcasting To")]
+        [SerializeField]
+        private VoidEventChannelSO _leaveGameToMainMenuEvent;
+
+        private void Start()
+        {
+            _toMainMenuBtn.onClick.AddListener(() => _leaveGameToMainMenuEvent.RaiseEvent());
+        }
 
         public void Show(int numberOfObjectsFixed)
         {
