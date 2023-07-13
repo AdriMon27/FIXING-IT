39a9359dc5d92e77998a821e15200d4c6d5903ef
Sadrmm <al394498@uji.es>
2023-07-13 16:49:34 +0200

    unload client scenes fixed?

--- FIXING IT/Assets/Scripts/MainMenu/MainMenuCleanUp.cs
+++ FIXING IT/Assets/Scripts/MainMenu/MainMenuCleanUp.cs
@@ -1,17 +1,36 @@
 using FixingIt.GameLobby;
 using FixingIt.Multiplayer;
 using FixingIt.SceneManagement.Logic;
+using FixingIt.SceneManagement.ScriptableObjects;
+using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace FixingIt.MainMenu
 {
     public class MainMenuCleanUp : MonoBehaviour
     {
+        [SerializeField]
+        private GameSceneSO[] _networkScenes; 
         // Code made to clean unnecesary network managers in MainMenu
         // All of them are loaded again at LobbySelection
         private void Awake()
         {
+            // unload unnecesary scenes
+            for (int i = 0; i < SceneManager.sceneCount; i++) {
+                foreach (GameSceneSO netScene in _networkScenes) {
+                    //Debug.LogWarning(netScene.name + " " + SceneManager.GetSceneAt(i));
+                    if (netScene.name == SceneManager.GetSceneAt(i).name) {
+                        SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(i));
+                        //Debug.LogWarning(i + SceneManager.GetSceneAt(i).name);
+                        //i--;
+                    }
+                }
+            }
+
+            // destroy unnecesary managers
             if (NetworkManager.Singleton != null)
             {
                 Destroy(NetworkManager.Singleton.gameObject);
